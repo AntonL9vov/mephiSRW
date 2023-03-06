20dff0b89be82c0f9d41059cc579557725228a43
yaakovlew <andrei252002@mail.ru>
2023-03-07 00:34:28 +0300

    Add option for DB

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
 	"mephiSRW"
 	"mephiSRW/pkg/handler"
 	"mephiSRW/pkg/repository"
 	"mephiSRW/pkg/service"
+	"os"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error intializing config: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env varibles: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("Fatal to connect to DB, because: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(mephiSRW.Server)
